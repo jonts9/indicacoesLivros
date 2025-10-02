b1b5f63b4c7df5ad07af75494dc6d1fe737441f1
Jonatas Costa <jonatascosta@tjsc.jus.br>
2025-10-02 10:52:45 -0300

    ocultar livros + novos

--- indicacoes.js
+++ indicacoes.js
@@ -26,27 +26,30 @@ fetch(path)
             grid.className = 'book-grid';
 
             livros.forEach(livro => {
-                const bookDiv = document.createElement('div');
-                bookDiv.className = 'book';
+                if(!livro.ocultar){
+                    cons = cont + 1;
+                    const bookDiv = document.createElement('div');
+                    bookDiv.className = 'book';
 
-                const bookTitle = `<a class="nome" href="${livro.url}" target="_blank">${livro.title}</a>`;
-                const bookLink = `<div class="link-full"><a class="link truncate-lines" href="${livro.url}" target="_blank">${livro.url}</a></div>`;
-                let qrCode = `<img class="qrcode" src="https://api.qrserver.com/v1/create-qr-code/?size=90x90&data=${encodeURIComponent(livro.url)}" alt="QR Code">`;
+                    const bookTitle = `<a class="nome" href="${livro.url}" target="_blank">${livro.title}</a>`;
+                    const bookLink = `<div class="link-full"><a class="link truncate-lines" href="${livro.url}" target="_blank">${livro.url}</a></div>`;
+                    let qrCode = `<img class="qrcode" src="https://api.qrserver.com/v1/create-qr-code/?size=90x90&data=${encodeURIComponent(livro.url)}" alt="QR Code">`;
 
-                if(!printQrCode) {
-                    qrCode = "";
+                    if(!printQrCode) {
+                        qrCode = "";
+                    }
+
+                    let bookImage = "";
+                    if (livro.imageUrl) {
+                        bookImage = `<div class="bookImage"><img src="${livro.imageUrl}"/></div>`;
+                    }
+                    let bookText = `<div>${bookTitle}${bookLink}</div>`
+                    let bookContainer = `<div class="bookContainer">${bookImage}<div class="bookText">${bookText}</div></div>`
+
+                    bookDiv.innerHTML = bookContainer;
+                    grid.appendChild(bookDiv);
+                    cont = cont + 1;
                 }
-
-                let bookImage = "";
-                if (livro.imageUrl) {
-                    bookImage = `<div class="bookImage"><img src="${livro.imageUrl}"/></div>`;
-                }
-                let bookText = `<div>${bookTitle}${bookLink}</div>`
-                let bookContainer = `<div class="bookContainer">${bookImage}<div class="bookText">${bookText}</div></div>`
-
-                bookDiv.innerHTML = bookContainer;
-                grid.appendChild(bookDiv);
-                cont = cont + 1;
             });
 
             groupDiv.appendChild(grid);
